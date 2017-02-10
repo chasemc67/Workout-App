6aae385a1a04a75a6af1c4a7618cdd6458706738
chasemc67 <chasemc67@gmail.com>
2017-02-10 15:10:36 -0700

    PDF Form is being generated and populated with TODOs now

--- pdf/generatePdf.py
+++ pdf/generatePdf.py
@@ -11,59 +11,158 @@ def generatePdfForPerson(person):
 	doc = SimpleDocTemplate('form.pdf', rightMarge=72, leftMargin=72, topMargin=72, bottomMargin=18)
 	Story=[]
 	logo = 'pdf/macewan-university-logo.png'		
-	magName = "Pythonista"
-	issueNum = 12
-	subPrice = "99.00"
-	limitedDate = "03/05/2010"
-	freeGift = "tin foil hat"
-
-	formatted_time = time.ctime()
-	full_name = "Mike Driscoll"
-	address_parts = ["411 State St", "Marshalltown, IA 50158"]
 
 	im = Image(logo, 4*inch, 2*inch)
 	Story.append(im)
 
 	styles = getSampleStyleSheet()
 	styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
-	ptext = '<font size=12>%s</font>' % formatted_time
+	ptext = '<font size=12>%s</font>' % person.testDate
 
 	Story.append(Paragraph(ptext, styles["Normal"]))
 	Story.append(Spacer(1, 12))
+	 
+	Story.append(Spacer(1, 12))
+	ptext = '<font size=12>Name %s:</font>' % person.name
+	Story.append(Paragraph(ptext, styles["Normal"]))
+	Story.append(Spacer(1, 12))
+	 
 
-	# Create return address
-	ptext = '<font size=12>%s</font>' % full_name
-	Story.append(Paragraph(ptext, styles["Normal"]))       
-	for part in address_parts:
-	    ptext = '<font size=12>%s</font>' % part.strip()
-	    Story.append(Paragraph(ptext, styles["Normal"]))   
-	 
-	Story.append(Spacer(1, 12))
-	ptext = '<font size=12>Dear %s:</font>' % full_name.split()[0].strip()
-	Story.append(Paragraph(ptext, styles["Normal"]))
-	Story.append(Spacer(1, 12))
-	 
-	ptext = '<font size=12>We would like to welcome you to our subscriber base for %s Magazine! \
-	        You will receive %s issues at the excellent introductory price of $%s. Please respond by\
-	        %s to start receiving your subscription and get the following free gift: %s.</font>' % (magName, 
-	                                                                                                issueNum,
-	                                                                                                subPrice,
-	                                                                                                limitedDate,
-	                                                                                                freeGift)
-	Story.append(Paragraph(ptext, styles["Justify"]))
-	Story.append(Spacer(1, 12))
-	 
-	 
-	ptext = '<font size=12>Thank you very much and we look forward to serving you.</font>'
-	Story.append(Paragraph(ptext, styles["Justify"]))
-	Story.append(Spacer(1, 12))
-	ptext = '<font size=12>Sincerely,</font>'
-	Story.append(Paragraph(ptext, styles["Normal"]))
-	Story.append(Spacer(1, 48))
-	ptext = '<font size=12>Ima Sucker</font>'
-	Story.append(Paragraph(ptext, styles["Normal"]))
-	Story.append(Spacer(1, 12))
+	if "BMIframe" in person.framesChecked: 
+		ptext = '<font size=12>BMI %s:</font>' % str(person.getBMI())
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
 
+	if "Circumference" in person.framesChecked:
+		ptext = '<font size=12>Circumference %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "Coopers" in person.framesChecked:
+		ptext = '<font size=12>Coopers %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "CurlUps" in person.framesChecked:
+		ptext = '<font size=12>CurlUps %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "DeepSquat" in person.framesChecked:
+		ptext = '<font size=12>DeepSquat %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "Ebelling" in person.framesChecked:
+		ptext = '<font size=12>Ebelling %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "FlexTests" in person.framesChecked:
+		ptext = '<font size=12>FlexTests %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "FrontPlank" in person.framesChecked:
+		ptext = '<font size=12>FrontPlank %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "GripStrength" in person.framesChecked:
+		ptext = '<font size=12>GripStrength %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "HipHinge" in person.framesChecked:
+		ptext = '<font size=12>HipHinge %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "MBtoss" in person.framesChecked:
+		ptext = '<font size=12>MBtoss %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "ModAst" in person.framesChecked:
+		ptext = '<font size=12>ModAst %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "PlankEnd" in person.framesChecked:
+		ptext = '<font size=12>PlankEnd %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "PushUps" in person.framesChecked:
+		ptext = '<font size=12>PushUps %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "RMpredict" in person.framesChecked:
+		ptext = '<font size=12>RMpredict %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "RMtest" in person.framesChecked:
+		ptext = '<font size=12>RMtest %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "Rockport" in person.framesChecked:
+		ptext = '<font size=12>Rockport %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "SLstance" in person.framesChecked:
+		ptext = '<font size=12>SLstance %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "SvnSiteSkinfold" in person.framesChecked:
+		ptext = '<font size=12>SvnSiteSkinfold %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "ThreeSiteSkinfold" in person.framesChecked:
+		ptext = '<font size=12>ThreeSiteSkinfold %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "VertJump" in person.framesChecked:
+		ptext = '<font size=12>VertJump %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "WallSit" in person.framesChecked:
+		ptext = '<font size=12>WallSit %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
+		
+
+	if "WallSlide" in person.framesChecked:
+		ptext = '<font size=12>WallSlide %s:</font>' % str("TODO fill in code")
+		Story.append(Paragraph(ptext, styles["Normal"]))
+		Story.append(Spacer(1, 12))
 
 
 	doc.build(Story)
